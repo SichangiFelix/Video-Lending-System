4b0f608931431106e28a89600a34512ebda61fe8
Sichangi prime <sichangifelix23@gmail.com>
2021-03-10 09:53:30 +0300

    edits

--- src/Graphics/SignUp.java
+++ src/Graphics/SignUp.java
@@ -359,7 +359,7 @@ public class SignUp extends javax.swing.JFrame {
                         .addGap(30, 30, 30)
                         .addComponent(dateLabel))
                     .addComponent(jDateChooser1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 35, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(signUpPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(genderLabel)
                     .addGroup(signUpPanelLayout.createSequentialGroup()
